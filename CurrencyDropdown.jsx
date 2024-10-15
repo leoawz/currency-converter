ca52e77dddfef714d9071789d31206cd66b71094
leoawz <leoawz@outlook.com>
2024-10-15 19:29:16 +0800

    Initial commit

---
+++ CurrencyDropdown.jsx
@@ -0,0 +1,6 @@
+<select name="currency" id="currencySelect">
+  <option value="usd">USD</option>
+  <option value="jpy">JPY</option>
+  <option value="sgd">SGD</option>
+  <option value="myr">MYR</option>
+</select>
